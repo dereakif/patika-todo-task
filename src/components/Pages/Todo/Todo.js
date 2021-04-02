19bb8ffa48653d6dd460c5efc7449eb4916ba480
Akif Dere <dereakif@gmail.com>
2021-04-03 01:29:50 +0300

    add: card logic, mock cards and create card

--- src/components/Pages/Todo/Todo.js
+++ src/components/Pages/Todo/Todo.js
@@ -3,30 +3,43 @@ import "./Todo.scss";
 const Todo = () => {
   let fName = localStorage.getItem("firstName");
   let lName = localStorage.getItem("lastName");
-  const [todos, setTodos] = useState([
-    ["asd", 2, 4, 5],
-    ["d", 2, 4, 1, 111],
-    [5],
-  ]);
+  const [todos, setTodos] = useState([]);
   const createHandler = () => {
-    setTodos([...setTodos, todos]);
+    setTodos([...todos, ["your next project", "asd", "125"]]);
+    console.log(todos);
   };
   return (
-    <div>
-      <div className="userInfoContainer"></div>
+    <div className="todoPageContainer">
+      <div className="column">
+        <div className="userInfoContainer">
+          <div
+            style={{
+              height: " 100px",
+              width: "100px",
+              border: "1px solid black",
+            }}
+          ></div>
+          <p>
+            {fName} {lName}
+          </p>
+        </div>
+      </div>
+
       <div className="cardContainer">
         {todos.length > 0 &&
           todos.map((todo, i) => (
             <div key={i} className="card">
               <ul>
-                {todo.map((listItem, index) => (
-                  <li key={index}>{listItem}</li>
-                ))}
+                {todo.length > 0 &&
+                  todo.map((listItem, index) => (
+                    <li key={index}>{listItem}</li>
+                  ))}
               </ul>
               <div className="cardBody"></div>
             </div>
           ))}
         <div className="createCard card">
+          <h1>YOUR NEXT PROJECT</h1>
           <button onClick={createHandler}>CREATE</button>
         </div>
       </div>
