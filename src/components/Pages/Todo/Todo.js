ec45ab493981f606aaee6850f9ddfbef55d9a667
Akif Dere <dereakif@gmail.com>
2021-04-03 01:02:26 +0300

    add: login img,  card style

--- src/components/Pages/Todo/Todo.js
+++ src/components/Pages/Todo/Todo.js
@@ -1,4 +1,34 @@
+import { useState } from "react";
+import "./Todo.scss";
 const Todo = () => {
-  return <div>hello from Todo</div>;
+  let fName = localStorage.getItem("firstName");
+  let lName = localStorage.getItem("lastName");
+  const [todos, setTodos] = useState([
+    ["asd", 2, 4, 5],
+    ["d", 2, 4, 1, 111],
+    [5],
+  ]);
+  const createHandler = () => {};
+  return (
+    <div>
+      <div className="userInfoContainer"></div>
+      <div className="cardContainer">
+        {todos.length > 0 &&
+          todos.map((todo, i) => (
+            <div key={i} className="card">
+              <ul>
+                {todo.map((listItem, index) => (
+                  <li key={index}>{listItem}</li>
+                ))}
+              </ul>
+              <div className="cardBody"></div>
+            </div>
+          ))}
+        <div className="createCard card">
+          <button onClick={createHandler}>CREATE</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Todo;
