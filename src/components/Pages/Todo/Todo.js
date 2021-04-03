89fa1530254dae731164cda817f9326181fd1587
Akif Dere <dereakif@gmail.com>
2021-04-03 04:10:24 +0300

    fixing todo list logic

--- src/components/Pages/Todo/Todo.js
+++ src/components/Pages/Todo/Todo.js
@@ -3,10 +3,25 @@ import "./Todo.scss";
 const Todo = () => {
   let fName = localStorage.getItem("firstName");
   let lName = localStorage.getItem("lastName");
-  const [todos, setTodos] = useState([]);
+  const [todoCards, setTodoCards] = useState([
+    [1, 2],
+    [3, 4],
+    [5, 6],
+  ]);
+  const [todoList, setTodoList] = useState([]);
+  const [todoItem, setTodoItem] = useState("");
   const createHandler = () => {
-    setTodos([...todos, ["your next project", "asd", "125"]]);
-    console.log(todos);
+    setTodoCards([...todoCards, []]);
+  };
+  const onChangeHandler = (e) => {
+    setTodoItem(e.target.value);
+    console.log(e.target.value);
+  };
+  const addTodo = (index) => {
+    setTodoList([...todoList, todoItem]);
+    //todoCards[index] = todoItem;
+
+    setTodoItem("");
   };
   return (
     <div className="todoPageContainer">
@@ -24,17 +39,49 @@ const Todo = () => {
           </p>
         </div>
       </div>
-
-      <div className="cardContainer">
-        {todos.length > 0 &&
-          todos.map((todo, i) => (
-            <div key={i} className="card">
-              <ul>
+      {/* 
+      <ul>
                 {todo.length > 0 &&
                   todo.map((listItem, index) => (
-                    <li key={index}>{listItem}</li>
+                    <div key={index} className="cardContent">
+                      <div className="cardTitle">
+                        <input></input>
+                      </div>
+                      <input></input>
+                      <li>{listItem}</li>
+                    </div>
                   ))}
-              </ul>
+              </ul> */}
+      <div className="cardContainer">
+        {todoCards.length > 0 &&
+          todoCards.map((todo, i) => (
+            <div key={i} className="card">
+              <div className="cardContent">
+                <div className="cardTitle">
+                  <input type="text"></input>
+                </div>
+                <input
+                  type="text"
+                  value={todoItem}
+                  onChange={onChangeHandler}
+                ></input>
+                <button onClick={() => addTodo(i)}>add</button>
+                {todo}
+                {todoList.length > 0 &&
+                  todoList.map((listItem, index) => (
+                    <div key={index}>
+                      <li
+                        style={{
+                          display: "flex",
+                        }}
+                      >
+                        <input type="checkbox" />
+                        <p>{listItem}</p>
+                        <button></button>
+                      </li>
+                    </div>
+                  ))}
+              </div>
               <div className="cardBody"></div>
             </div>
           ))}
