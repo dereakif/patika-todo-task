72d8b19868797d854853c6b313bcc2f7dcf55a2d
Akif Dere <dereakif@gmail.com>
2021-04-04 16:03:49 +0300

    add: category logic and id logic to cards

--- src/components/Pages/Todo/Todo.js
+++ src/components/Pages/Todo/Todo.js
@@ -4,16 +4,30 @@ const Todo = () => {
   let fName = localStorage.getItem("firstName");
   let lName = localStorage.getItem("lastName");
   const [todoCards, setTodoCards] = useState([]);
+  const [cardId, setCardId] = useState(0);
   const [todoItem, setTodoItem] = useState([]);
-  //const [todoList, setTodoList] = useState([]);
+  const [category, setCategory] = useState("");
+  const [categories, setCategories] = useState([]);
+  const [selectedCategoryList, setSelectedCategoryList] = useState([]);
+
   useEffect(() => {
     console.log(todoCards, "cards", "useefect");
     console.log(todoItem, "todoItem useeffetc");
-  }, [todoItem, todoCards]);
+    console.log(todoCards, "testingggg");
+    console.log(cardId, "cardId");
+    //console.log(selectedCategoryList, "selectedCategoryList");
+    //console.log(category, "category");
+    //console.log(categories, "categories");
+  }, [todoItem, todoCards, selectedCategoryList, cardId]);
   const createHandler = () => {
-    let cards = [...todoCards];
-    let card = { todos: [] };
-    setTodoCards([...cards, card]);
+    if (selectedCategoryList.length === 0) {
+      alert("first select a category");
+    } else {
+      let card = { todos: [], category: selectedCategoryList, id: cardId };
+      let cards = [...todoCards];
+      setTodoCards([...cards, card]);
+      setCardId([...cards, card].length);
+    }
   };
   const onChangeHandler = (e, index) => {
     let itemList = [...todoItem];
@@ -23,6 +37,7 @@ const Todo = () => {
     setTodoItem(itemList);
   };
   const addTodo = (index) => {
+    console.log(selectedCategoryList);
     let cards = [...todoCards];
     let card = cards[index];
     let todo = todoItem[index];
@@ -54,6 +69,10 @@ const Todo = () => {
     let cards = [...todoCards];
     cards.splice(cardIndex, 1);
     setTodoCards(cards);
+    let newTodoItems = [...todoItem];
+    newTodoItems[cardIndex] = "";
+    setTodoItem(newTodoItems);
+    setCardId(cards.length);
   };
   const checkBoxHandler = (cardIndex, todoIndex) => {
     let cards = [...todoCards];
@@ -66,6 +85,31 @@ const Todo = () => {
     card.todos = cardTodos;
     cards[cardIndex] = card;
     setTodoCards(cards);
+  };
+  const categoryOnChangeHandler = (e) => {
+    setCategory(e.target.value);
+  };
+  const addCategory = () => {
+    if (category === "" || category === undefined) {
+      alert("please add a category");
+    } else {
+      let newCategories = [...categories, category];
+      setCategories(newCategories);
+      setCategory("");
+    }
+  };
+  const categoryCheckBoxHandler = (index) => {
+    let selectedCategory = categories[index];
+    let newSelectedCategoryList = selectedCategoryList.filter(
+      (item) => item !== selectedCategory
+    );
+    console.log(categories);
+    !selectedCategoryList.includes(selectedCategory)
+      ? setSelectedCategoryList([...selectedCategoryList, selectedCategory])
+      : setSelectedCategoryList(newSelectedCategoryList);
+
+    /*  !selectedCategoryList.includes(selectedCategory) &&
+      setSelectedCategoryList([...selectedCategoryList, selectedCategory]); */
   };
   return (
     <div className="todoPageContainer">
@@ -82,69 +126,94 @@ const Todo = () => {
             {fName} {lName}
           </p>
         </div>
+        <div className="categorySection">
+          <h2>Categories</h2>
+          <input onChange={categoryOnChangeHandler} value={category}></input>
+          <button onClick={addCategory}>add</button>
+          <div className="categoryList">
+            {categories.length > 0 &&
+              categories.map((category, index) => (
+                <div className="categoryContainer">
+                  <input
+                    type="checkbox"
+                    onClick={() => categoryCheckBoxHandler(index)}
+                  ></input>
+                  <p className="category">{category}</p>
+                </div>
+              ))}
+          </div>
+          <p></p>
+        </div>
       </div>
 
       <div className="cardContainer">
         {todoCards.length > 0 &&
-          todoCards.map((todoCard, cardIndex) => (
-            <div key={cardIndex} className="card">
-              <div className="cardContent">
-                <div
-                  className="closeBtnCotainer"
-                  style={{
-                    display: "flex",
-                    justifyContent: "flex-end",
-                  }}
-                >
+          selectedCategoryList.length > 0 &&
+          todoCards
+            .filter((card) => {
+              return card.category.every((category) =>
+                selectedCategoryList.includes(category)
+              );
+            })
+            .map((todoCard, cardIndex) => (
+              <div key={cardIndex} className="card">
+                <div className="cardContent">
                   <div
-                    className="closeBtn"
-                    style={{ cursor: "pointer", width: "max-content" }}
-                    onClick={() => closeHandler(cardIndex)}
+                    className="closeBtnCotainer"
+                    style={{
+                      display: "flex",
+                      justifyContent: "flex-end",
+                    }}
                   >
-                    &#10006;
+                    <div
+                      className="closeBtn"
+                      style={{ cursor: "pointer", width: "max-content" }}
+                      onClick={() => closeHandler(cardIndex)}
+                    >
+                      &#10006;
+                    </div>
+                  </div>
+                  <div className="cardTitle">
+                    <input type="text"></input>
+                  </div>
+                  <input
+                    type="text"
+                    value={todoItem[cardIndex]}
+                    onChange={(e) => onChangeHandler(e, cardIndex)}
+                  ></input>
+                  <button onClick={() => addTodo(cardIndex)}>add</button>
+                  <div style={{ overflowY: "auto", height: "200px" }}>
+                    {todoCard.todos.map((item, todoIndex) => (
+                      <div
+                        key={todoIndex}
+                        style={{ display: "flex", alignItems: "center" }}
+                      >
+                        <input
+                          type="checkbox"
+                          onClick={() => checkBoxHandler(cardIndex, todoIndex)}
+                        />
+                        <p
+                          style={{
+                            textDecoration: item.isChecked
+                              ? "line-through"
+                              : "none",
+                          }}
+                        >
+                          {item.todo}
+                        </p>
+                        <button
+                          onClick={() => deleteHandler(cardIndex, todoIndex)}
+                          style={{ height: "max-content" }}
+                        >
+                          &times;
+                        </button>
+                      </div>
+                    ))}
                   </div>
                 </div>
-                <div className="cardTitle">
-                  <input type="text"></input>
-                </div>
-                <input
-                  type="text"
-                  value={todoItem[cardIndex]}
-                  onChange={(e) => onChangeHandler(e, cardIndex)}
-                ></input>
-                <button onClick={() => addTodo(cardIndex)}>add</button>
-                <div style={{ overflowY: "auto", height: "200px" }}>
-                  {todoCard.todos.map((item, todoIndex) => (
-                    <div
-                      key={todoIndex}
-                      style={{ display: "flex", alignItems: "center" }}
-                    >
-                      <input
-                        type="checkbox"
-                        onClick={() => checkBoxHandler(cardIndex, todoIndex)}
-                      />
-                      <p
-                        style={{
-                          textDecoration: item.isChecked
-                            ? "line-through"
-                            : "none",
-                        }}
-                      >
-                        {item.todo}
-                      </p>
-                      <button
-                        onClick={() => deleteHandler(cardIndex, todoIndex)}
-                        style={{ height: "max-content" }}
-                      >
-                        &times;
-                      </button>
-                    </div>
-                  ))}
-                </div>
+                <div className="cardBody"></div>
               </div>
-              <div className="cardBody"></div>
-            </div>
-          ))}
+            ))}
         <div className="createCard card">
           <h1>YOUR NEXT PROJECT</h1>
           <button onClick={createHandler}>CREATE</button>
