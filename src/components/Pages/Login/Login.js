1aa6f38699a4f42db01a36f844db294eaf3abe5b
Akif Dere <dereakif@gmail.com>
2021-04-06 14:40:49 +0300

    add: login page input font color, adjust: media queries

--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -6,22 +6,33 @@ import Button from "@material-ui/core/Button";
 import LoginImg from "../../../assets/pixel.png";
 import "./Login.scss";
 import { TextField } from "@material-ui/core";
-import { useState } from "react";
+import { withStyles } from "@material-ui/core/styles";
+import { useEffect, useState } from "react";
 
-const Login = () => {
+const styles = {
+  root: {
+    borderColor: "white",
+  },
+  input: {
+    color: "#310c40",
+  },
+};
+const Login = (props) => {
+  const { classes } = props;
   let history = useHistory();
 
-  const [firstName, setfirstName] = useState("");
-  const [lastName, setlastName] = useState("");
+  const [firstName, setfirstName] = useState(undefined);
+  const [lastName, setlastName] = useState(undefined);
+  useEffect(() => {
+    console.log(firstName, lastName);
+  }, [firstName, lastName]);
 
   const firstNameHandler = (e) => {
     setfirstName(e.target.value);
-    //localStorage.setItem("firstName", e.target.value);
   };
 
   const lastNameHandler = (e) => {
     setlastName(e.target.value);
-    //localStorage.setItem("lastName", e.target.value);
   };
 
   const loginOnClickHandler = () => {
@@ -45,37 +56,77 @@ const Login = () => {
           <div className="header"></div>
           <div className="inputTitle"></div>
           <div className="input">
-            <TextField
-              onChange={firstNameHandler}
-              onKeyDown={enterHandler}
-              value={firstName}
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              label="First Name"
-              name="firtName"
-              autoComplete="first name"
-              autoFocus
-            />
+            {firstName === "" ? (
+              <TextField
+                error
+                required
+                fullWidth
+                label="First Name"
+                value={firstName}
+                onChange={firstNameHandler}
+                variant="outlined"
+                margin="normal"
+                className={classes.root}
+                InputProps={{
+                  className: classes.input,
+                }}
+              />
+            ) : (
+              <TextField
+                onChange={firstNameHandler}
+                onKeyDown={enterHandler}
+                value={firstName}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="First Name"
+                name="firtName"
+                autoComplete="first name"
+                autoFocus
+                InputProps={{
+                  className: classes.input,
+                }}
+              />
+            )}
           </div>
           <div className="inputTitle"></div>
           <div className="input">
-            <TextField
-              onChange={lastNameHandler}
-              onKeyDown={enterHandler}
-              value={lastName}
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              label="Last Name"
-              name="firtName"
-              autoComplete="first name"
-            />
+            {lastName === "" ? (
+              <TextField
+                error
+                required
+                fullWidth
+                label="Last Name"
+                value={lastName}
+                onChange={lastNameHandler}
+                variant="outlined"
+                margin="normal"
+                InputProps={{
+                  className: classes.input,
+                }}
+              />
+            ) : (
+              <TextField
+                onChange={lastNameHandler}
+                onKeyDown={enterHandler}
+                value={lastName}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                label="Last Name"
+                name="firtName"
+                autoComplete="first name"
+                InputProps={{
+                  className: classes.input,
+                }}
+              />
+            )}
           </div>
 
           <Button
+            style={{ marginTop: "1rem" }}
             variant="contained"
             color="primary"
             onClick={loginOnClickHandler}
@@ -88,4 +139,4 @@ const Login = () => {
     </div>
   );
 };
-export default Login;
+export default withStyles(styles)(Login);
